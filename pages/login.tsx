c396564df34c759b335a3b391fbeb548e3bc9315
jonghyeon <cjddnjfql@naver.com>
2023-08-09 15:01:59 +0900

    feat: isLogin atom

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,15 +1,18 @@
 import Layout from '@/components/common/Layout';
 import useAuthKakao from '@/hooks/useAuthKakao';
 import useLocalStorage from '@/hooks/useLocalStorage';
+import { isLoginState } from '@/recoil/atoms';
 import { profile } from '@/types';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
 function Login() {
   const { login } = useAuthKakao();
   const router = useRouter();
   const [token, setToken] = useLocalStorage<string>('token');
   const [profile, setProfile] = useLocalStorage<profile>('profile');
+  const [isLogin, setIsLogin] = useRecoilState(isLoginState);
 
   const handleLogin = async () => {
     const { code } = router.query;
@@ -18,6 +21,7 @@ function Login() {
       const res = await login(code);
       setToken(res.token);
       setProfile(res.user);
+      setIsLogin(true);
       router.push('/');
     }
   };
