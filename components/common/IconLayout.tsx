74d08a6ca73ac3dbf33505393e2b738eb83c9482
jonghyeon <cjddnjfql@naver.com>
2023-08-10 16:34:00 +0900

    feat: input value 삭제버튼

--- components/common/IconLayout.tsx
+++ components/common/IconLayout.tsx
@@ -1,12 +1,13 @@
 type Props = {
+  className?: string;
   children: React.ReactNode;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-function IconLayout({ children, onClick }: Props) {
+function IconLayout({ children, className, onClick }: Props) {
   return (
     <button
-      className="w-[36px] h-[36px] flex items-center justify-center"
+      className={`w-[36px] h-[36px] flex items-center justify-center ${className}`}
       type="button"
       onClick={onClick}
     >
