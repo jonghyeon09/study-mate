0dabcce5598001448a3a7cc645abe1636d819ca3
jonghyeon <cjddnjfql@naver.com>
2023-08-12 17:23:29 +0900

    fix: 로그인 버그 수정

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,11 +4,9 @@ import useLocalStorage from '@/hooks/useLocalStorage';
 import { profile } from '@/types';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { setToken } from '@/lib/cookies';
+import { setToken, token } from '@/lib/cookies';
 import { useRecoilState } from 'recoil';
 import { isLoginState } from '@/recoil/atoms';
-import { useQuery } from '@tanstack/react-query';
-import { getStudyList } from '@/services';
 import Splash from '@/components/Splash/Splash';
 
 export default function Login() {
@@ -20,42 +18,49 @@ export default function Login() {
     profileImage: '',
     lastAccessedStudyId: 0,
   });
-  const { isLoading, data: studyList } = useQuery({
-    queryKey: ['studyList'],
-    queryFn: getStudyList,
-    enabled: isLogin,
-  });
-
-  const handleLogin = async () => {
-    const { code } = router.query;
-
-    if (code) {
-      const res = await login(code);
-      const newUser = res.isNew;
-
-      setToken(res.token);
-      setProfile(res.user);
-      setIsLogin(true);
-
-      if (newUser) router.push('/welcome');
-    }
-  };
 
   useEffect(() => {
-    if (!isLogin) {
+    const handleLogin = async () => {
+      if (isLogin) return;
+      const { code } = router.query;
+
+      if (code) {
+        const res = await login(code);
+        const newUser = res.isNew;
+
+        setToken(res.token);
+        setProfile(res.user);
+        setIsLogin(true);
+
+        if (newUser) router.push('/welcome');
+      }
+    };
+
+    if (!token) {
       handleLogin();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router, isLogin]);
+  }, [isLogin, login, router, setIsLogin, setProfile]);
 
   useEffect(() => {
-    if (!studyList) return;
-    if (studyList.study.length !== 0) {
-      router.push(`/study/${studyList.userId}`);
-    } else {
-      router.push(`/welcome`);
+    if (token) {
+      setIsLogin(true);
     }
-  }, [studyList, router]);
+  }, [router, setIsLogin]);
+
+  useEffect(() => {
+    if (isLogin) {
+      router.push('/');
+    }
+  }, [isLogin, router]);
+
+  // useEffect(() => {
+  //   if (!studyList) return;
+  //   if (studyList.study.length !== 0) {
+  //     router.push(`/study/${studyList.userId}`);
+  //   } else {
+  //     router.push(`/welcome`);
+  //   }
+  // }, [studyList, router]);
 
   return (
     <Layout>
