857d4761933fec25b8361a2991ec199fae43c2fd
jonghyeon <cjddnjfql@naver.com>
2023-08-14 21:10:02 +0900

    chore: pwa 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -17,6 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
+          <Head>
+            <link rel="manifest" href="/manifest.json" />
+          </Head>
           <Component {...pageProps} />
         </Hydrate>
         <ReactQueryDevtools initialIsOpen={false} />
