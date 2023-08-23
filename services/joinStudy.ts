b294d3d8a095f4112a3b5622972d1e7562c4c44f
jonghyeon <cjddnjfql@naver.com>
2023-08-23 17:03:47 +0900

    fix: 스터디 참여 에러핸들링

--- services/joinStudy.ts
+++ services/joinStudy.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import apiClient from './apiClient';
 
 type Data = {
@@ -11,7 +12,14 @@ export const joinStudy = async ({ data }: Data) => {
     const response = await apiClient.post<{ studyId: string }>(`/invite`, data);
     return response.data;
   } catch (error) {
-    alert('스터디 참여 실패');
+    if (axios.isAxiosError(error)) {
+      error.status === 400
+        ? alert('이미 참여중 이거나 만료된 초대코드 입니다')
+        : null;
+    } else {
+      alert('스터디 참여 실패');
+    }
+
     throw error;
   }
 };
