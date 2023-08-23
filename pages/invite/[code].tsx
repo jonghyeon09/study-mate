47eeef41a8670250979a7d25005bbed9055ffa93
jonghyeon <cjddnjfql@naver.com>
2023-08-23 16:24:19 +0900

    feat: 스터디 참가

--- pages/invite/[code].tsx
+++ pages/invite/[code].tsx
@@ -2,17 +2,47 @@ import { useEffect } from 'react';
 import useLocalStorage from '@/hooks/useLocalStorage';
 import { useRouter } from 'next/router';
 import Splash from '@/components/Splash/Splash';
+import { joinStudy } from '@/services/joinStudy';
+import { useQuery } from '@tanstack/react-query';
+import { getStudyList } from '@/services';
 
 export default function Invite() {
   const [token] = useLocalStorage('token', '');
   const { query, push } = useRouter();
   const code = typeof query.code === 'string' ? query.code : undefined;
+  const { data: studyList } = useQuery({
+    queryKey: ['studyList'],
+    queryFn: getStudyList,
+    enabled: !!token,
+  });
 
   useEffect(() => {
     if (!token) {
       push('/');
     }
   }, [push, token]);
+
+  useEffect(() => {
+    if (!code) return;
+
+    const handleJoin = async () => {
+      const { studyId } = await joinStudy({
+        data: {
+          code,
+        },
+      });
+
+      push({
+        pathname: '/study/[id]',
+        query: {
+          id: studyList?.userId,
+          study: studyId,
+        },
+      });
+    };
+
+    handleJoin();
+  }, [code, push, studyList?.userId]);
 
   return <Splash />;
 }
