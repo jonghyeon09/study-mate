28fb554fe7c9ead03939451954c0a7505ea5e507
jonghyeon <cjddnjfql@naver.com>
2023-08-24 16:37:43 +0900

    feat: 공지사항 작성 api

---
+++ services/createNotice.ts
@@ -0,0 +1,24 @@
+import apiClient from './apiClient';
+
+type Data = {
+  params: {
+    studyId: string;
+  };
+  data: {
+    tag: string;
+    description: string;
+  };
+};
+
+export const deleteStudy = async ({ params, data }: Data) => {
+  try {
+    const response = await apiClient.post(
+      `/study/${params.studyId}/notice`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    alert('등록 실패');
+    throw error;
+  }
+};
