a0ea94dceca47eff80bb6282e113bf0804b306ab
jonghyeon <cjddnjfql@naver.com>
2023-08-29 18:01:59 +0900

    design: 이미지 품질 수정

--- components/Splash/Splash.tsx
+++ components/Splash/Splash.tsx
@@ -13,7 +13,7 @@ function Splash() {
         transition={{ duration: 0.5 }}
         className="flex items-center justify-center absolute w-full h-full bg-[--color-indigo]"
       >
-        <Image alt="logo" src={logo} />
+        <Image alt="logo" src={logo} quality={100} />
       </motion.div>
     </PopupLayout>
   );
